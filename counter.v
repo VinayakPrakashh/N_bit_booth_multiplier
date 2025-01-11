5430d0b00383e5cb2a73a1424c00cfe4a09d0530
Vinayak Prakash <vinayakprakash2121@gmail.com>
2025-01-11 12:24:37 +0530

    d

--- counter.v
+++ counter.v
@@ -3,7 +3,7 @@ module counter #(parameter N = 4) (
     output reg [N-1:0] data_out
 );
     always @(posedge clk) begin
-        if(ld) data_out <= {N{1'b0}} + 1;
+        if(ld) data_out <= N;
         else if(dec) data_out <= data_out - 1;
     end
 endmodule
